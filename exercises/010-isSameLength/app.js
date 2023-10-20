f256a6e47d25808021be1213edffd5017a5be07c
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-10-20 18:45:09 +0000

    Ejercicios

--- exercises/010-isSameLength/app.js
+++ exercises/010-isSameLength/app.js
@@ -1,1 +1,4 @@
-// Write your function here
+function isSameLength(word1, word2) {
+    // Compara las longitudes de las dos palabras y retorna true si son iguales, de lo contrario, retorna false.
+    return word1.length === word2.length;
+  }
